07a6672870f891e385482f3ff67c0a059f765975
Stephen <sndegwa.n@outlook.com>
2020-09-29 15:29:49 +0300

    Set react error boundaries

---
+++ src/app/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import {Wrapper, Info} from "./styles";
+
+class ErrorBoundary extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      hasError:false,
+      error:""
+    }
+  }
+  
+  static getDerivedStateFromError(error) {
+    return {
+      hasError:true, 
+      error:error.message||"An unexpected error occured."
+    };  
+  }
+  
+  componentDidCatch(error, errorInfo) {    
+    console.log(error);  
+  }
+  
+  render(){
+    let {hasError, error} = this.state;
+    if(hasError){
+      return (<Wrapper>
+        <Info>
+          <div>{error}</div>
+          <div><a href="/" alt="Homepage"><button>Go to home</button></a></div>
+        </Info>
+      </Wrapper>);
+    }else{
+      return this.props.children;
+    }
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/app/components/ErrorBoundary/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+width:100%;
+height:100%;
+background-color:white;
+display:flex;
+justify-content:center;
+align-items:center;
+`;
+
+export const Info = styled.div`
+width:250px;
+line-height:30px;
+padding:1em;
+font-size:1.2em;
+background-color:#eeeeee;
+border:1px solid #bdbdbd;
+border-radius:5px;
+text-align:center;
+`;
